54e0b28577ef11a046c4fb54d7b41cc04e43b837
Raul Maulidhino <raulahmadm314@gmail.com>
2025-06-30 23:28:17 +0700

    feat: add 30% off discount Promo component

---
+++ src/components/Home/Promo.svelte
@@ -0,0 +1,7 @@
+<section class="bg-[url('/images/services/hanging-clothes.webp')] bg-no-repeat bg-fixed bg-center my-6 bg-primary text-secondary flex flex-wrap gap-4">
+  <div class="bg-black/50 h-full w-full px-12 py-24 text-primary">
+    <h2 class="text-3xl font-bold md:text-4xl mb-[0.75em]">Dapatkan Diskon hingga 30%!</h2>
+    <p class="mb-[1em] max-w-sm">Hemat hingga <strong>30%</strong> untuk setiap desain custom Anda. Temukan penawaran terbaik kami di sini!</p>
+    <button class="text-primary bg-accent border-2 border-accent font-semibold w-full md:w-fit px-4 py-2 rounded-lg transition ease-in-out duration-500 hover:text-accent hover:bg-primary hover:shadow-lg hover:scale-[0.95]">Order Sekarang!</button>
+  </div>
+</section>
