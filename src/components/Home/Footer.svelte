bf3b9780b918d2d74cc70f3c65d1718d1399b2db
Raul Maulidhino <raulahmadm314@gmail.com>
2025-06-30 23:29:20 +0700

    feat: add Footer component

---
+++ src/components/Home/Footer.svelte
@@ -0,0 +1,64 @@
+<footer class="pt-[5rem] px-6 md:px-8 bg-accent text-primary">
+  <section class="flex flex-wrap justify-between gap-6">
+    <div class="max-w-sm space-y-6">
+      <img src="/images/logos/Custom-Style-logo_white.png" alt="Custom Style Logo" class="max-w-[90%]" />
+      <p>Dapatkan gaya yang sesuai dengan kepribadian Anda di Custom Style! Pakaian berkualitas tinggi dengan sentuhan desain inovatif, eksklusif untuk Anda.</p>
+      <p>Temukan kreativitas Anda dalam setiap jahitan. Mulailah perjalanan gaya pribadi Anda sekarang!</p>
+      <ul class="flex gap-4">
+        <li>
+          <a href="https://facebook.com" target="_blank" title="Our Official Facebook Account" rel="noopener noreferrer">
+            <img src="https://cdn.simpleicons.org/facebook/FFFFFF" alt="Facebook" class="w-6 h-6 hover:scale-[1.25] hover:drop-shadow-xl transition duration-500 ease" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/cstmstyle" target="_blank" title="Our Official Instagram Account" rel="noopener noreferrer">
+            <img src="https://cdn.simpleicons.org/instagram/FFFFFF" alt="Instagram" class="w-6 h-6 hover:scale-[1.25] hover:drop-shadow-xl transition duration-500 ease" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/@custom_style" target="_blank" title="Our Official YouTube Channel" rel="noopener noreferrer">
+            <img src="https://cdn.simpleicons.org/youtube/FFFFFF" alt="YouTube" class="w-6 h-6 hover:scale-[1.25] hover:drop-shadow-xl transition duration-500 ease" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.whatsapp.com" target="_blank" title="Our Business WhatsApp Number" rel="noopener noreferrer">
+            <img src="https://cdn.simpleicons.org/whatsapp/FFFFFF" alt="WhatsApp" class="w-6 h-6 hover:scale-[1.25] hover:drop-shadow-xl transition duration-500 ease" />
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="flex flex-wrap gap-4 max-w-sm">
+      <div>
+        <h3 class="text-lg font-bold">Informasi</h3>
+        <ul>
+          <li><a href="/">Beranda</a></li>
+          <li><a href="/">Hubungi Kami</a></li>
+          <li><a href="/">Kebijakan Privasi</a></li>
+          <li><a href="/">Customer Service</a></li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="text-lg font-bold">Kategori</h3>
+        <ul>
+          <li><a href="/">Jaket, Hoodie, & Almamater</a></li>
+          <li><a href="/">Jersey Olahraga</a></li>
+          <li><a href="/">Kaos Oblong</a></li>
+          <li><a href="/">Seragam Kantor</a></li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="text-lg font-bold">Hubungi Kami</h3>
+        <ul>
+          <li>Telepon: (+62) 877 1404 7526</li>
+          <li>Butuh bantuan atau pertanyaan?</li>
+          <li>Hubungi kami di: <a href="mailto:info@cstmstyle.com" target="_blank" class="underline font-bold">info@cstmstyle.com</a></li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <section class="py-8 text-center">
+    <p>
+      &copy; {new Date().getFullYear()} <strong>Custom Style</strong>. All rights reserved.
+    </p>
+  </section>
+</footer>
