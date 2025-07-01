0f17cdb2ba816ce630d8bf1eb0cf6fdf3c4f4292
Raul Maulidhino <raulahmadm314@gmail.com>
2025-07-01 21:27:06 +0700

    feat: optimize SEO by adding high-volume keywords to title, meta description, Hero and CTA buttons

--- src/components/Home/Promo.svelte
+++ src/components/Home/Promo.svelte
@@ -2,6 +2,6 @@
   <div class="bg-black/50 h-full w-full px-12 py-24 text-primary">
     <h2 class="text-3xl font-bold md:text-4xl mb-[0.75em]">Dapatkan Diskon hingga 30%!</h2>
     <p class="mb-[1em] max-w-sm">Hemat hingga <strong>30%</strong> untuk setiap desain custom Anda. Temukan penawaran terbaik kami di sini!</p>
-    <button class="text-primary bg-accent border-2 border-accent font-semibold w-full md:w-fit px-4 py-2 rounded-lg transition ease-in-out duration-500 hover:text-accent hover:bg-primary hover:shadow-lg hover:scale-[0.95]">Order Sekarang!</button>
+    <button class="text-primary bg-accent border-2 border-accent font-semibold w-full md:w-fit px-4 py-2 rounded-lg transition ease-in-out duration-500 hover:text-accent hover:bg-primary hover:shadow-lg hover:scale-[0.95]">Pesan Sekarang!</button>
   </div>
 </section>
