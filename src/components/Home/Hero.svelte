e1774511919fec360f597e3e6090071e09f8377c
Raul Maulidhino <raulahmadm314@gmail.com>
2025-07-01 17:03:40 +0700

    style: resolve line height and maximum width of subtitle

--- src/components/Home/Hero.svelte
+++ src/components/Home/Hero.svelte
@@ -1,8 +1,8 @@
 <section class="flex flex-wrap-reverse">
   <section class="w-full md:w-[calc(50vw-2rem)] grow p-8 flex justify-center items-center">
     <div>
-      <h1 class="text-4xl font-bold md:text-5xl lg:text-6xl leading-14 mb-[0.5em] ">Bebas <span class="bg-accent text-white px-1">Desain</span>.<br /> Bebas <span class="underline decoration-accent">Gaya</span>.<br /> 100% Custom.</h1>
-      <p class="text-lg md:text-xl lg:text-2xl mb-[1em]">Pengerjaan cepat, pelayanan personal, dan berkualitas tinggi.</p>
+      <h1 class="text-4xl font-bold md:text-5xl lg:text-6xl leading-14 lg:leading-[4rem] mb-[0.5em] ">Bebas <span class="bg-accent text-white px-1">Desain</span>.<br /> Bebas <span class="underline decoration-accent">Gaya</span>.<br /> 100% Custom.</h1>
+      <p class="text-lg md:text-xl lg:text-2xl mb-[1em] max-w-sm">Pengerjaan cepat, pelayanan personal, dan berkualitas tinggi.</p>
       <div class="flex flex-wrap gap-3">
         <button class="md:text-lg text-primary bg-accent border-2 border-accent font-semibold w-full md:w-fit px-4 py-2 rounded-lg transition ease-in-out duration-500 hover:shadow-lg hover:scale-[0.95]">Order Sekarang</button>
         <button class="md:text-lg text-accent bg-primary border-2 border-accent font-semibold w-full md:w-fit px-4 py-2 rounded-lg transition ease-in-out duration-500 hover:shadow-lg hover:scale-[0.95]">Lihat Produk</button>  
